3103c4e54343977d3726dffcd08ca3b05f64c301
Alvaro Armijos <alvaro.armijoss@gmail.com>
2025-07-03 20:30:55 -0500

    Runnin info widget and markers.

---
+++ lib/ui/map/widgets/floating_actions.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tracking_eela_2025/ui/map/bloc/location_bloc/location_bloc.dart';
+import 'package:tracking_eela_2025/ui/map/bloc/map_cubit/map_cubit.dart';
+
+class FloatingActions extends StatelessWidget {
+  const FloatingActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mapCubit = context.read<MapCubit>();
+    final locationBloc = context.read<LocationBloc>();
+    
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton.small(
+            onPressed: () {
+              locationBloc.add(ToggleShowLocationEvent());
+            },
+            child: Icon(Icons.more_vert),
+          ),
+          FloatingActionButton.small(
+            onPressed: () {
+              final lastKnownLocation = locationBloc
+                  .state
+                  .lastKnownLocation;
+              if (lastKnownLocation == null) return;
+              mapCubit.moveCamera(lastKnownLocation);
+            },
+            child: Icon(Icons.my_location),
+          ),
+        ],
+      );
+  }
+}
