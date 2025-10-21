6676f0fa617a519600ddcd5dde31a755cd4041af
Alvaro Armijos <alvaro.armijoss@gmail.com>
2025-10-21 17:53:31 -0500

    test

--- lib/ui/map/view/map_page.dart
+++ lib/ui/map/view/map_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Map page ....')));
+    return const Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-3.997864, -79.200924),
+          zoom: 17,
+        ),
+        zoomControlsEnabled: false,
+        myLocationEnabled: true,
+      ),
+    );
   }
 }
